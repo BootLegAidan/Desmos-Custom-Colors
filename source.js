393d530352f9f698cf8830e1a0d11f34c3e01667
xDERPYxCREEPERx <30330512+xDERPYxCREEPERx@users.noreply.github.com>
2020-03-27 13:08:37 -0500

    Update source.js

--- source.js
+++ source.js
@@ -2,6 +2,6 @@ window.alert(`color([hex value], [position in list])`)
 var tmpvar;
 function color(hex, index){
   tmpvar = Calc.getState();
-  tmpvar.expressions.list[(index + 1)].color = hex;
+  tmpvar.expressions.list[(index - 1)].color = hex;
   Calc.setState(tmpvar);
 };
